1bdc024a250cafbba83ed6246136ac56c49651cb
Adrian Sampson <adrian@radbox.org>
2016-01-07 15:39:01 -0800

    Command-line options

--- spliterate.js
+++ spliterate.js
@@ -1,6 +1,8 @@
 var remark = require('remark');
 var visit = require('unist-util-visit');
 var remove = require('unist-util-remove');
+var program = require('commander');
+var fs = require('fs');
 
 function spliterator(code_out) {
   // Our Remark "plugin".
@@ -19,9 +21,27 @@ function spliterator(code_out) {
 var code_chunks = [];
 var plugin = spliterator(code_chunks);
 
+program
+  .option('-m --markdown <file>', 'markdown output file')
+  .option('-c --code <file>', 'code output file')
+  .parse(process.argv);
+
 // Process the document.
 var processor = remark().use(plugin);
-var doc = processor.process('hello\nfoo\n\n    bar\n\nbaz *qux*');
+var markdown = processor.process('hello\nfoo\n\n    bar\n\nbaz *qux*');
+var code = code_chunks.join('\n') + '\n';
 
-console.log(code_chunks);
-console.log(doc);
+// Output Markdown.
+if (program.markdown) {
+  fs.writeFile(program.markdown, markdown);
+}
+
+// Output extracted code.
+if (program.code) {
+  fs.writeFile(program.code, code);
+}
+
+// If neither is specified, just dump the code to stdout.
+if (!program.markdown && !program.code) {
+  process.stdout.write(code);
+}
